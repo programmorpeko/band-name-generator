507526d4e06bf9d0cb40fdf7c4e428267e6f9bcc
programmorpeko <105959515+programmorpeko@users.noreply.github.com>
2022-05-20 22:45:02 -0500

    Add main.py

---
+++ main.py
@@ -0,0 +1,7 @@
+print("Welcome to the Band Name Generator.")
+
+city_name = input("Which city did you grow up in?\n")
+pet_name = input("What is the name of your pet?\n")
+band_name = city_name + " " + pet_name
+
+print("Your band name could be " + band_name + ".")
